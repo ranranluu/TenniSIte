ef059a43efe75738a228ab85772170f04fee5c6e
ranranluu <ranranluutennis@gmail.com>
2021-08-10 06:35:18 +0000

    [add]モデル等の追加

---
+++ db/migrate/20210810045808_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.text :content, null: false
+      t.string :image
+      t.timestamps
+    end
+  end
+end
