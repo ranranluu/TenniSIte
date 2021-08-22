42932ebd3ae7df95333617c3ed244ff19dd10f01
ranranluu <ranranluutennis@gmail.com>
2021-08-22 12:30:41 +0000

    [add]comment

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resource :likes, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
 
   resources :tags, except: [:index, :create, :new, :edit, :show, :update, :destroy] do
